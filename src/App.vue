2c89cf1da7d621b24b41b3dd46ec0568f871cdb8
Keyla Bullón <katla7775@gmail.com>
2022-06-28 16:39:17 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>pinia init store in vuejs</h1>
+  <router-link to='/'>Home</router-link>
+  <router-link to='/counter'>Counter</router-link>
+  <router-view></router-view>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
